279b53d2b2692cf2e107ab9055140618598d6b98
marcosradix <marcosradix@gmail.com>
2018-05-17 12:00:53 -0300

    projeto leitor de qrcode flutter dart

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String barcode = "";
+  @override
+  initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+          appBar: new AppBar(
+            backgroundColor: Colors.amberAccent,
+            title: new Text('Leitor QRCODE'),
+          ),
+          body: new Center(
+            child: new Column(
+              children: <Widget>[
+                new IconButton(
+                  icon: new Icon(
+                      const IconData(0xe8fc, fontFamily: 'MaterialIcons')),
+                  tooltip: 'Ler código',
+                  onPressed: () {
+                    scan();
+                  },
+                ),
+                new TextField(
+                  controller: new TextEditingController.fromValue(
+                      new TextEditingValue(text: barcode)),
+                  style: Theme.of(context).textTheme.body2,
+                  decoration: new InputDecoration(
+                    hintText: 'aqui os dados.',
+                    labelText: "Código lido",
+                    labelStyle: Theme.of(context).textTheme.display1,
+                    contentPadding: new EdgeInsets.all(8.0),
+                  ),
+                ),
+                new Divider(),
+                new RaisedButton(
+                  color: Colors.amberAccent,
+                  child: new Text("Copiar"),
+                  onPressed: () {
+                  Clipboard.setData(new ClipboardData(text: barcode));
+                  new AlertDialog(
+                    title: new Text("Texto copiado"),
+                    content: new Text(this.barcode),
+                    
+                  );
+                  },
+                )
+              ],
+            ),
+          )),
+    );
+  }
+
+  Future scan() async {
+    try {
+      String barcode = await BarcodeScanner.scan();
+      setState(() => this.barcode = barcode);
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          this.barcode = 'Usuário não concedeu permissão para a camera';
+        });
+      } else {
+        setState(() => this.barcode = 'Unknown error: $e');
+      }
+    } on FormatException {
+      setState(() => this.barcode = '(Não foi possível ler o QRCode)');
+    } catch (e) {
+      setState(() => this.barcode = 'Unknown error: $e');
+    }
+ 
+  }
+  showDialog(){
+    
+  }
+
+}
